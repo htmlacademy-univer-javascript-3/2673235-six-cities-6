3a5af9dc4d9e2b208324059a9bfcfd5dd385d86c
Robert Malikov <robmalikov@gmail.com>
2025-10-09 05:57:03 +0500

    2.13. Начинаем программировать

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainPage from './pages/MainPage';
+
+type AppProps = {
+  offersCount: number;
+};
+
+export default function App({ offersCount }: AppProps) {
+  return <MainPage offersCount={offersCount} />;
+}
