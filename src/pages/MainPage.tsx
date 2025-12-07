f4ca2390d3c68de66aa0196fa6f243a1f1ecd4b7
Robert Malikov <robmalikov@gmail.com>
2025-12-07 06:19:26 +0500

    6.14. Контроль и ограничения (часть 1)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,31 @@
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../store';
+import { changeCity } from '../store/action';
+import type { City } from '../store/reducer';
 import OffersList from '../components/OffersList';
 import Map from '../components/Map';
+import CitiesList from '../components/CitiesList';
 import type { Offer } from '../mocks/offers';
 
-type MainPageProps = {
-  offers: Offer[];
-};
+export default function MainPage() {
+  const dispatch = useDispatch();
+  const city = useSelector((s: RootState) => s.city);
+  const allOffers = useSelector((s: RootState) => s.offers);
 
-export default function MainPage({ offers }: MainPageProps) {
-  const amsterdamOffers = offers.filter((o) => o.city === 'Amsterdam');
+  const cities: City[] = useMemo(
+    () => ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'],
+    [],
+  );
+
+  const filtered: Offer[] = useMemo(
+    () => allOffers.filter((o) => o.city === city),
+    [allOffers, city],
+  );
+
+  const onSelectCity = (c: City) => {
+    dispatch(changeCity(c));
+  };
 
   return (
     <div className="page page--gray page--main">
@@ -22,16 +40,14 @@ export default function MainPage({ offers }: MainPageProps) {
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#todo">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
                     <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                     <span className="header__favorite-count">3</span>
                   </a>
                 </li>
                 <li className="header__nav-item">
-                  <a className="header__nav-link" href="#todo">
-                    <span className="header__signout">Sign out</span>
-                  </a>
+                  <a className="header__nav-link" href="#"><span className="header__signout">Sign out</span></a>
                 </li>
               </ul>
             </nav>
@@ -42,24 +58,15 @@ export default function MainPage({ offers }: MainPageProps) {
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
 
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item"><a className="locations__item-link tabs__item" href="#todo"><span>Paris</span></a></li>
-              <li className="locations__item"><a className="locations__item-link tabs__item" href="#todo"><span>Cologne</span></a></li>
-              <li className="locations__item"><a className="locations__item-link tabs__item" href="#todo"><span>Brussels</span></a></li>
-              <li className="locations__item"><a className="locations__item-link tabs__item tabs__item--active" href="#todo"><span>Amsterdam</span></a></li>
-              <li className="locations__item"><a className="locations__item-link tabs__item" href="#todo"><span>Hamburg</span></a></li>
-              <li className="locations__item"><a className="locations__item-link tabs__item" href="#todo"><span>Dusseldorf</span></a></li>
-            </ul>
-          </section>
-        </div>
+        <CitiesList cities={cities} currentCity={city} onSelect={onSelectCity} />
 
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{amsterdamOffers.length} places to stay in Amsterdam</b>
+              <b className="places__found">
+                {filtered.length} places to stay in {city}
+              </b>
 
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
@@ -77,12 +84,12 @@ export default function MainPage({ offers }: MainPageProps) {
                 </ul>
               </form>
 
-              <OffersList offers={amsterdamOffers} />
+              <OffersList offers={filtered} />
             </section>
 
             <div className="cities__right-section">
               <section className="cities__map map">
-                <Map offers={amsterdamOffers} />
+                <Map offers={filtered} />
               </section>
             </div>
           </div>
