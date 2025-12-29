db4da60bbcec35235ab31704530120ce0b7a2cc3
Robert Malikov <robmalikov@gmail.com>
2025-12-29 12:01:40 +0500

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-/// <reference types='vitest' />
-/// <reference types='vite/client' />
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = {
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
-});
+};
+
+export default defineConfig(config);
